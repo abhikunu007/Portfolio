0d472ad0b14a16089aecd348b8570d06580e31ab
abhikunu007 <ak0879918@gmail.com>
2023-09-01 15:26:08 +0530

    completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './modules/components/home/home.component';
+import { ContactComponent } from './modules/components/contact/contact.component';
+import { ServiceComponent } from './modules/components/service/service.component';
+import { ProjectComponent } from './modules/components/project/project.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'project', component: ProjectComponent },
+  { path: 'service', component: ServiceComponent  },
+  { path: 'contact', component: ContactComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactComponent } from './modules/components/contact/contact.component';
+import { ServiceComponent } from './modules/components/service/service.component';
+import { NavbarComponent } from './modules/components/navbar/navbar.component';
+import { FooterComponent } from './modules/components/footer/footer.component';
+import { HomeComponent } from './modules/components/home/home.component';
+import { FormsModule } from '@angular/forms';
+import { ProjectComponent } from './modules/components/project/project.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactComponent,
+    ServiceComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    ProjectComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
